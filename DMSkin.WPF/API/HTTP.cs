6a1ae5af70d020972908f4e80e781185ad7ac8a0
DMSkin <www.944095635@qq.com>
2018-06-14 00:34:53 +0800

    更新一些文件

--- DMSkin.WPF/API/HTTP.cs
+++ DMSkin.WPF/API/HTTP.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
-using System.Linq;
 using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
