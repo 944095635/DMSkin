1f3ed4913a269c39bf3e1b726855289d7f1a8476
Dream.Machine <944095635@qq.com>
2018-06-18 20:27:44 +0800

    加入了一些新的元素

--- DMSkin.WPF/Controls/DMTextBox.cs
+++ DMSkin.WPF/Controls/DMTextBox.cs
@@ -34,6 +34,33 @@ namespace DMSkin.WPF.Controls
         }
         public static readonly DependencyProperty ShowIconProperty =
             DependencyProperty.Register("ShowIcon", typeof(bool), typeof(DMTextBox), new PropertyMetadata(false));
+
+
+        /// <summary>
+        /// 图标宽度
+        /// </summary>
+        public double IconWidth
+        {
+            get { return (double)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register("IconWidth", typeof(double), typeof(DMTextBox), new PropertyMetadata(15.0));
+
+
+
+        /// <summary>
+        /// 图标高度
+        /// </summary>
+        public double IconHeight
+        {
+            get { return (double)GetValue(IconHeightProperty); }
+            set { SetValue(IconHeightProperty, value); }
+        }
+        public static readonly DependencyProperty IconHeightProperty =
+            DependencyProperty.Register("IconHeight", typeof(double), typeof(DMTextBox), new PropertyMetadata(15.0));
+
+
         //public double ShadowBlurRadius
         //{
         //    get { return (double)GetValue(ShadowBlurRadiusProperty); }
