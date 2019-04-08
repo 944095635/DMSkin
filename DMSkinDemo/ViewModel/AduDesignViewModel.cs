a011fac868e0119f8b91bd90db562349e78ee6b8
非完美 <1280429435@qq.com>
2019-04-09 00:07:23 +0800

    XX

---
+++ DMSkinDemo/ViewModel/AduDesignViewModel.cs
@@ -0,0 +1,52 @@
+﻿using DMSkin.Core.Common;
+using DMSkin.Core.MVVM;
+using DMSkinDemo.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DMSkinDemo.ViewModel
+{
+    public class AduDesignViewModel:ViewModelBase
+    {
+        #region 初始化
+        public AduDesignViewModel()
+        {
+            Execute.OnUIThread(() =>
+            {
+                CodeList = new ObservableCollection<DMCode> {
+                    new DMCode() {  CodeID=1,CodeName="DMSkin"}
+                    ,new DMCode() {  CodeID=1,CodeName="AduSkin"}
+                };
+            }, false);
+        }
+        #endregion
+
+        #region 属性
+        private ObservableCollection<DMCode> _CodeList;
+        /// <summary>
+        /// CodeList
+        /// </summary>
+        public ObservableCollection<DMCode> CodeList
+        {
+            get { return _CodeList; }
+            set
+            {
+                _CodeList = value;
+                OnPropertyChanged(nameof(CodeList));
+            }
+        }
+
+        #endregion
+
+        #region 命令
+        /// <summary>
+        /// 测试按钮
+        /// </summary>    
+        public ICommand TestCommand => new DelegateCommand(obj =>
+        {
+            MessageBox.Show("DMSkin");
+        });
+        #endregion
+    }
+}
