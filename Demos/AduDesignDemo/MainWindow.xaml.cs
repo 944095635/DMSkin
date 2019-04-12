002dc69eac4cb610ff5abaf65ee33da964be2028
非完美 <1280429435@qq.com>
2019-04-13 01:03:34 +0800

    新增控件组

--- Demos/AduDesignDemo/MainWindow.xaml.cs
+++ Demos/AduDesignDemo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using AduDesignDemo.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace AduDesignDemo
 {
@@ -7,6 +9,21 @@ namespace AduDesignDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DMSkinWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<DMCode> CodeList = new ObservableCollection<DMCode> {
+                    new DMCode() {  CodeID=1,CodeName="AduSkin",Phone="1870921****",Email="1840921****@qq.com",Info="追求极致，永臻完美"}
+                    ,new DMCode() {  CodeID=1,CodeName="DMSkin",Phone="1840921****",Email="1840921****@qq.com",Info="梦机器"}
+                    ,new DMCode() {  CodeID=1,CodeName="AduSkin",Phone="1870921****",Email="1840921****@qq.com",Info="追求极致，永臻完美"}
+                    ,new DMCode() {  CodeID=1,CodeName="DMSkin",Phone="1840921****",Email="1840921****@qq.com",Info="梦机器"}
+                     ,new DMCode() {  CodeID=1,CodeName="AduSkin",Phone="1870921****",Email="1840921****@qq.com",Info="追求极致，永臻完美"}
+                    ,new DMCode() {  CodeID=1,CodeName="DMSkin",Phone="1840921****",Email="1840921****@qq.com",Info="梦机器"}
+                    ,new DMCode() {  CodeID=1,CodeName="AduSkin",Phone="1870921****",Email="1840921****@qq.com",Info="追求极致，永臻完美"}
+                    ,new DMCode() {  CodeID=1,CodeName="AduSkin",Phone="1870921****",Email="1840921****@qq.com",Info="追求极致，永臻完美"}
+                };
+            AduDataGrids.ItemsSource = CodeList;
         }
     }
 }
--- Demos/AduDesignDemo/Model/DMCode.cs
+++ Demos/AduDesignDemo/Model/DMCode.cs
@@ -4,31 +4,12 @@ namespace AduDesignDemo.Model
 {
     public class DMCode:ViewModelBase
     {
-        private int _CodeID;
-        /// <summary>
-        /// CodeID
-        /// </summary>
-        public int CodeID
-        {
-            get { return _CodeID; }
-            set
-            {
-                _CodeID = value;
-                OnPropertyChanged(nameof(CodeID));
-            }
-        }
-        private string _CodeName;
-        /// <summary>
-        /// CodeName
-        /// </summary>
-        public string CodeName
-        {
-            get { return _CodeName; }
-            set
-            {
-                _CodeName = value;
-                OnPropertyChanged(nameof(CodeName));
-            }
-        }
+        public bool IsChecked { get; set; }
+        public string CodeType { get; set; }
+        public int CodeID { get; set; }
+        public string CodeName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Info { get; set; }
     }
 }
