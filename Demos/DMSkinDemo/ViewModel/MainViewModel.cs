4921c96509aa77857625d97602c3edd80d5ece6b
DMSkin <944095635@qq.com>
2019-04-12 21:19:19 +0800

    x

--- Demos/DMSkinDemo/ViewModel/MainViewModel.cs
+++ Demos/DMSkinDemo/ViewModel/MainViewModel.cs
@@ -1,11 +1,33 @@
 ﻿using DMSkin.Core;
 using DMSkinDemo.Model;
 using DMSkinDemo.View;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DMSkinDemo.ViewModel
 {
     class MainViewModel:ViewModelBase
     {
+        public MainViewModel()
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            Task task = Task.Run(async ()=> 
+            {
+                while (tokenSource.IsCancellationRequested)
+                {
+                    await Task.Delay(1000);
+                    System.Console.WriteLine("");
+                }
+            });
+
+            //立即取消
+            tokenSource.Cancel();
+
+            //3秒之后取消
+            tokenSource.CancelAfter(3000);
+        }
+
         #region 属性
         private Menu selectMenu;
         /// <summary>
