8d3a5e0e73accefae45893680338cb8bae9d6099
DMSkin <944095635@qq.com>
2019-04-13 23:13:39 +0800

    x

---
+++ DMSkin.Core/Converters/CompareToVisibilityConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using Converts = System.Convert;
+
+namespace DMSkin.Core.Converters
+{
+    /// <summary>
+    /// 比较数字大小 返回是否显示的转换器
+    /// 例：当界面尺寸大于某个值的时候显示某些东西。
+    /// </summary>
+    public class CompareToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// 转换函数
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double v1 = Converts.ToDouble(value);
+                double v2 = Converts.ToDouble(parameter);
+                if (v1 > v2)
+                {
+                    return Visibility.Visible;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ DMSkin.Core/Converters/SecondToStringConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace DMSkin.Core.Converters
+{
+    /// <summary>
+    /// 将秒数 转换 为时间显示 00:10:00
+    /// </summary>
+    public class SecondToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int offset)
+            {
+                var absOffset = Math.Abs(offset);
+                var hour = ((absOffset / 3600)).ToString();
+                var minute = ((absOffset - ((absOffset / 3600) * 3600)) / 60).ToString();
+                var second = ((absOffset - ((absOffset / 3600) * 3600)) % 60).ToString();
+                return $"{(offset >= 0 ? "" : "-")}{hour.PadLeft(2, '0')}:{minute.PadLeft(2, '0')}:{second.PadLeft(2, '0')}";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var arr = (value as string)?.Split(':');
+            if (arr?.Length == 2)
+            {
+                long hour, minute;
+                if (Int64.TryParse(arr[0], out hour) && Int64.TryParse(arr[1], out minute))
+                {
+                    return hour * 3600 + minute * 60;
+                }
+            }
+            return value;
+        }
+    }
+}
