c04b813606ed75fec65a7d40dd53e5d049eae528
非完美 <1280429435@qq.com>
2019-04-23 23:34:51 +0800

    AduSkin改动

--- Demos/AduDesignDemo/Properties/AssemblyInfo.cs
+++ Demos/AduDesignDemo/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("AduDesignDemo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Design By Adu")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AduSkin")]
 [assembly: AssemblyProduct("AduDesignDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright Adu©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
